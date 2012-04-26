ba83b497b2d52086b2afe410ab166812ade33f9c
Armon Dadgar <armon.dadgar@gmail.com>
2012-04-26 16:06:38 -0700

    Adding a timer module

---
+++ src/timer.c
@@ -0,0 +1,128 @@
+#include <math.h>
+#include "timer.h"
+
+/* Static declarations */
+static void finalize_timer(timer *timer);
+
+/**
+ * Initializes the timer struct
+ * @arg eps The maximum error for the quantiles
+ * @arg quantiles A sorted array of double quantile values, must be on (0, 1)
+ * @arg num_quants The number of entries in the quantiles array
+ * @arg timeer The timer struct to initialize
+ * @return 0 on success.
+ */
+int init_timer_quantile(double eps, double *quantiles, uint32_t num_quants, timer *timer) {
+    timer->count = 0;
+    timer->sum = 0;
+    timer->squared_sum = 0;
+    timer->finalized = 1;
+    int res = init_cm_quantile(eps, quantiles, num_quants, &timer->cm);
+    return res;
+}
+
+/**
+ * Destroy the timer struct.
+ * @arg timer The timer to destroy
+ * @return 0 on success.
+ */
+int destroy_timer(timer *timer) {
+    return destroy_cm_quantile(&timer->cm);
+}
+
+/**
+ * Adds a new sample to the struct
+ * @arg timer The timer to add to
+ * @arg sample The new sample value
+ * @return 0 on success.
+ */
+int timer_add_sample(timer *timer, double sample) {
+    timer->count += 1;
+    timer->sum += sample;
+    timer->squared_sum += pow(sample, 2);
+    timer->finalized = 0;
+    return cm_add_sample(&timer->cm, sample);
+}
+
+/**
+ * Queries for a quantile value
+ * @arg timer The timer to query
+ * @arg quantile The quantile to query
+ * @return The value on success or 0.
+ */
+double timer_query(timer *timer, double quantile) {
+    finalize_timer(timer);
+    return cm_query(&timer->cm, quantile);
+}
+
+/**
+ * Returns the number of samples in the timer
+ * @arg timer The timer to query
+ * @return The number of samples
+ */
+uint64_t timer_count(timer *timer) {
+    return timer->count;
+}
+
+/**
+ * Returns the minimum timer value
+ * @arg timer The timer to query
+ * @return The number of samples
+ */
+double timer_min(timer *timer) {
+    finalize_timer(timer);
+    if (!timer->cm.samples) return 0;
+    return timer->cm.samples->value;
+}
+
+/**
+ * Returns the mean timer value
+ * @arg timer The timer to query
+ * @return The mean value
+ */
+double timer_mean(timer *timer) {
+    return timer->sum / timer->count;
+}
+
+/**
+ * Returns the sample standard deviation timer value
+ * @arg timer The timer to query
+ * @return The sample standard deviation
+ */
+double timer_stddev(timer *timer) {
+    double num = (timer->count * timer->squared_sum) - pow(timer->sum, 2);
+    double div = timer->count * (timer->count - 1);
+    return sqrt(num / div);
+}
+
+/**
+ * Returns the sum of the timer
+ * @arg timer The timer to query
+ * @return The sum of values
+ */
+double timer_sum(timer *timer) {
+    return timer->sum;
+}
+
+/**
+ * Returns the maximum timer value
+ * @arg timer The timer to query
+ * @return The maximum value
+ */
+double timer_max(timer *timer) {
+    finalize_timer(timer);
+    if (!timer->cm.end) return 0;
+    return timer->cm.end->value;
+}
+
+// Finalizes the timer for queries
+static void finalize_timer(timer *timer) {
+    if (timer->finalized) return;
+
+    // Force the quantile to flush internal
+    // buffers so that queries are accurate.
+    cm_flush(&timer->cm);
+
+    timer->finalized = 1;
+}
+
---
+++ src/timer.h
@@ -0,0 +1,89 @@
+#ifndef TIMER_H
+#define TIMER_H
+#include <stdint.h>
+#include "cm_quantile.h"
+
+typedef struct {
+    uint64_t count;     // Count of items
+    double sum;         // Sum of the values
+    double squared_sum; // Sum of the squared values
+    int finalized;      // Is the cm_quantile finalized
+    cm_quantile cm;     // Quantile we use
+} timer;
+
+/**
+ * Initializes the timer struct
+ * @arg eps The maximum error for the quantiles
+ * @arg quantiles A sorted array of double quantile values, must be on (0, 1)
+ * @arg num_quants The number of entries in the quantiles array
+ * @arg timeer The timer struct to initialize
+ * @return 0 on success.
+ */
+int init_timer_quantile(double eps, double *quantiles, uint32_t num_quants, timer *timer);
+
+/**
+ * Destroy the timer struct.
+ * @arg timer The timer to destroy
+ * @return 0 on success.
+ */
+int destroy_timer(timer *timer);
+
+/**
+ * Adds a new sample to the struct
+ * @arg timer The timer to add to
+ * @arg sample The new sample value
+ * @return 0 on success.
+ */
+int timer_add_sample(timer *timer, double sample);
+
+/**
+ * Queries for a quantile value
+ * @arg timer The timer to query
+ * @arg quantile The quantile to query
+ * @return The value on success or 0.
+ */
+double timer_query(timer *timer, double quantile);
+
+/**
+ * Returns the number of samples in the timer
+ * @arg timer The timer to query
+ * @return The number of samples
+ */
+uint64_t timer_count(timer *timer);
+
+/**
+ * Returns the minimum timer value
+ * @arg timer The timer to query
+ * @return The number of samples
+ */
+double timer_min(timer *timer);
+
+/**
+ * Returns the mean timer value
+ * @arg timer The timer to query
+ * @return The mean value
+ */
+double timer_mean(timer *timer);
+
+/**
+ * Returns the sample standard deviation timer value
+ * @arg timer The timer to query
+ * @return The sample standard deviation
+ */
+double timer_stddev(timer *timer);
+
+/**
+ * Returns the sum of the timer
+ * @arg timer The timer to query
+ * @return The sum of values
+ */
+double timer_sum(timer *timer);
+
+/**
+ * Returns the maximum timer value
+ * @arg timer The timer to query
+ * @return The maximum value
+ */
+double timer_max(timer *timer);
+
+#endif
