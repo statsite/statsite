9f128a7ee0ece5883be2973e94740186f5324712
Armon Dadgar <armon.dadgar@gmail.com>
2012-04-27 11:39:52 -0700

    Filling out metrics

--- src/metrics.c
+++ src/metrics.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include "metrics.h"
 
+static int counter_delete_cb(void *data, const char *key, void *value);
+static int timer_delete_cb(void *data, const char *key, void *value);
+
 /**
  * Initializes the metrics struct.
  * @arg eps The maximum error for the quantiles
@@ -42,7 +45,30 @@ int init_metrics_defaults(metrics *m) {
  * Destroys the metrics
  * @return 0 on success.
  */
-int destroy_metrics(metrics *m);
+int destroy_metrics(metrics *m) {
+    // Clear the copied quantiles array
+    free(m->quantiles);
+
+    // Nuke all the k/v pairs
+    key_val *current = m->kv_vals;
+    key_val *prev = NULL;
+    while (current) {
+        free(current->name);
+        prev = current;
+        current = current->next;
+        free(prev);
+    }
+
+    // Nuke the counters
+    hashmap_iter(m->counters, counter_delete_cb, NULL);
+    hashmap_destroy(m->counters);
+
+    // Nuke the timers
+    hashmap_iter(m->timers, timer_delete_cb, NULL);
+    hashmap_destroy(m->timers);
+
+    return 0;
+}
 
 /**
  * Increments the counter with the given name
@@ -51,7 +77,20 @@ int destroy_metrics(metrics *m);
  * @arg val The value to add
  * @return 0 on success
  */
-int metrics_increment_counter(metrics *m, char *name, double val);
+int metrics_increment_counter(metrics *m, char *name, double val) {
+    counter *c;
+    int res = hashmap_get(m->counters, name, (void**)&c);
+
+    // New counter
+    if (res == -1) {
+        c = malloc(sizeof(counter));
+        init_counter(c);
+        hashmap_put(m->counters, name, c);
+    }
+
+    // Add the sample value
+    return counter_add_sample(c, val);
+}
 
 /**
  * Adds a new timer sample for the timer with a
@@ -60,7 +99,20 @@ int metrics_increment_counter(metrics *m, char *name, double val);
  * @arg val The sample to add
  * @return 0 on success.
  */
-int metrics_add_timer_sample(metrics *m, char *name, double val);
+int metrics_add_timer_sample(metrics *m, char *name, double val) {
+    timer *t;
+    int res = hashmap_get(m->timers, name, (void**)&t);
+
+    // New timer
+    if (res == -1) {
+        t = malloc(sizeof(timer));
+        init_timer(m->eps, m->quantiles, m->num_quants, t);
+        hashmap_put(m->timers, name, t);
+    }
+
+    // Add the sample value
+    return timer_add_sample(t, val);
+}
 
 /**
  * Adds a new K/V pair
@@ -68,6 +120,27 @@ int metrics_add_timer_sample(metrics *m, char *name, double val);
  * @arg val The value associated
  * @return 0 on success.
  */
-int metrics_add_kv(metrics *m, char *name, double val);
+int metrics_add_kv(metrics *m, char *name, double val) {
+    key_val *kv = malloc(sizeof(key_val));
+    kv->name = strdup(name);
+    kv->val = val;
+    kv->next = m->kv_vals;
+    m->kv_vals = kv;
+    return 0;
+}
 
+
+// Counter map cleanup
+static int counter_delete_cb(void *data, const char *key, void *value) {
+    free(value);
+    return 0;
+}
+
+// Timer map cleanup
+static int timer_delete_cb(void *data, const char *key, void *value) {
+    timer *t = value;
+    destroy_timer(t);
+    free(t);
+    return 0;
+}
 
