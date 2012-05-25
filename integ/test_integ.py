7d704371d01c617f0a810559bb47bf27ecf7a986
Armon Dadgar <armon.dadgar@gmail.com>
2012-05-25 16:51:51 -0700

    Adding UDP integration tests

--- integ/test_integ.py
+++ integ/test_integ.py
@@ -1,6 +1,5 @@
 import os
 import os.path
-import shutil
 import socket
 import subprocess
 import sys
@@ -30,8 +29,9 @@ def pytest_funcarg__servers(request):
     conf = """[statsite]
 flush_interval = 1
 port = %d
+udp_port = %d
 stream_cmd = %s
-""" % (port, cmd)
+""" % (port, port, cmd)
     open(config_path, "w").write(conf)
 
     # Start the process
@@ -68,8 +68,7 @@ stream_cmd = %s
         raise EnvironmentError("Failed to connect!")
 
     # Make a second connection
-    conn2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn2.settimeout(1)
+    conn2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     conn2.connect(("localhost", port))
 
     # Return the connection
@@ -116,6 +115,49 @@ class TestInteg(object):
         assert "timers.noobs.median|49.000000" in out
         assert "timers.noobs.p95|95.000000" in out
         assert "timers.noobs.p99|99.000000" in out
+
+
+class TestIntegUDP(object):
+    def test_kv(self, servers):
+        "Tests adding kv pairs"
+        _, server, output = servers
+        server.sendall("tubez:100|kv\n")
+        time.sleep(1)
+        now = time.time()
+        out = open(output).read()
+        assert out in ("kv.tubez|100.000000|%d\n" % now, "kv.tubez|100.000000|%d\n" % (now - 1))
+
+    def test_counters(self, servers):
+        "Tests adding kv pairs"
+        _, server, output = servers
+        server.sendall("foobar:100|c\n")
+        server.sendall("foobar:200|c\n")
+        server.sendall("foobar:300|c\n")
+        time.sleep(1)
+        now = time.time()
+        out = open(output).read()
+        assert out in ("counts.foobar|600.000000|%d\n" % now, "counts.foobar|600.000000|%d\n" % (now - 1))
+
+    def test_meters(self, servers):
+        "Tests adding kv pairs"
+        _, server, output = servers
+        msg = ""
+        for x in xrange(100):
+            msg += "noobs:%d|ms\n" % x
+        server.sendall(msg)
+        time.sleep(1)
+        out = open(output).read()
+        print out
+        assert "timers.noobs.sum|4950" in out
+        assert "timers.noobs.mean|49.500000" in out
+        assert "timers.noobs.lower|0.000000" in out
+        assert "timers.noobs.upper|99.000000" in out
+        assert "timers.noobs.count|100" in out
+        assert "timers.noobs.stdev|29.011492" in out
+        assert "timers.noobs.median|49.000000" in out
+        assert "timers.noobs.p95|95.000000" in out
+        assert "timers.noobs.p99|99.000000" in out
+
 
 if __name__ == "__main__":
     sys.exit(pytest.main(args="-k TestInteg."))
