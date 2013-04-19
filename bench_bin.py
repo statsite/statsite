82f65161dba8b19f4499a6fbab63cd77daf1bdae
Armon Dadgar <armon.dadgar@gmail.com>
2013-04-18 17:01:55 -0700

    Update the binary benchmark to support sets

--- bench_bin.py
+++ bench_bin.py
@@ -8,7 +8,8 @@ KEYS = ["test", "foobar", "zipzap"]
 VALS = [32, 100, 82, 101, 5, 6, 42, 73]
 
 BINARY_HEADER = struct.Struct("<BBHd")
-BIN_TYPES = {"kv": 1, "c": 2, "ms": 3}
+BINARY_SET_HEADER = struct.Struct("<BBHH")
+BIN_TYPES = {"kv": 1, "c": 2, "ms": 3, "set": 4}
 
 
 def format(key, type, val):
@@ -21,11 +22,24 @@ def format(key, type, val):
     return mesg
 
 
+def format_set(key, val):
+    "Formats a binary set message for statsite"
+    key = str(key)
+    key_len = len(key) + 1
+    val = str(val)
+    val_len = len(val) + 1
+
+    type_num = BIN_TYPES["set"]
+    header = BINARY_SET_HEADER.pack(170, type_num, key_len, val_len)
+    mesg = "".join([header, key, "\0", val, "\0"])
+    return mesg
+
+
 METS = []
 for x in xrange(NUM):
     key = random.choice(KEYS)
-    val = random.choice(VALS)
-    METS.append(format(key, "c", val))
+    val = str(x) #random.choice(VALS)
+    METS.append(format_set(key, val))
 
 s = socket.socket()
 s.connect(("localhost", 8125))
