1abb1cdb89baff610c412a9adad0ead2e5285607
John Keates <john@keates.nl>
2016-03-21 04:19:51 +0100

    Update bootstrap script and gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,20 @@
 #! /bin/sh
 
 set -x
-mkdir m4 ac_config
+
+# Create directories in case they are missing
+mkdir -p m4 ac_config
+
+
 aclocal -I ac_config
-#libtoolize --force --copy
-#autoheader
+
+# Execute libtoolize if we have it
+command -v libtoolize >/dev/null 2>&1 && libtoolize --force --copy
+
+# Execute glibtoolize as that's what OSX has, usually
+command -v glibtoolize >/dev/null 2>&1 && glibtoolize --force --copy
+
+#autoheader # We dont need this (yet?)
+
 automake --add-missing --copy
 autoconf
