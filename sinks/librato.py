19d5e7b0b7daa56027175ef09d7d81e20f152751
Bryan Mikaelian <bryan@librato.com>
2016-09-26 14:26:44 -0500

    Import librato changes for tagging support
    Add support for tagging measurements
    
    Build the measurement and send it to Librato
    
    Always write to /v1/measurements. Write to /v1/metrics if config is set
    
    Got this thing working and submitting data
    
    Clean up. Removing write_to_legacy logic for now
    
    Finish up legacy writing support
    
    Remove dual write option
    
    Revert "Remove dual write option"
    
    This reverts commit 36d8556dbb48ab5805bf7d013a90ea8ce886246c.
    
    Write to legacy true
    
    Top leel tag support. Rename source to host

--- sinks/librato.py
+++ sinks/librato.py
@@ -1,6 +1,7 @@
 """
 Supports flushing statsite metrics to Librato
 """
+import ast
 import sys
 import socket
 import logging
@@ -63,11 +64,12 @@ class LibratoStore(object):
         self.parse_conf(conffile)
 
         self.sink_name = "statsite-librato"
-        self.sink_version = "0.0.1"
+        self.sink_version = "1.0.1"
 
         self.flush_timeout_secs = 5
         self.gauges = {}
-
+        self.measurements = {}
+        
         # Limit our payload sizes
         self.max_metrics_payload = 500
 
@@ -77,9 +79,9 @@ class LibratoStore(object):
 
         self.sfx_map = {
             'sum': 'sum',
-            'sum_sq' : 'sum_squares',
+            'sum_sq': None,
             'count' : 'count',
-            'stdev' : None,
+            'stdev' : 'stddev_m2',
             'lower' : 'min',
             'upper' : 'max',
             'mean' : None
@@ -116,7 +118,12 @@ class LibratoStore(object):
         if config.has_option(sect, 'source'):
             self.source = config.get(sect, 'source')
         else:
-            self.source = socket.gethostname()
+            self.source = None
+        
+        if config.has_option(sect, 'host'):
+            self.host = config.get(sect, 'host')
+        else:
+            self.host = socket.gethostname()
 
         if config.has_option(sect, 'source_regex'):
             reg = config.get(sect, 'source_regex')
@@ -148,6 +155,18 @@ class LibratoStore(object):
             self.extended_counters = config.getboolean(sect, "extended_counters")
         else:
             self.extended_counters = False
+        
+        # Check if we want to also send measurements to legacy Librato API
+        if config.has_option(sect, "write_to_legacy"):
+            self.write_to_legacy = config.getboolean(sect, "write_to_legacy")
+        else:
+            self.write_to_legacy = False
+        
+        # Global Tags
+        if config.has_option(sect, "tags"):
+            self.tags = ast.literal_eval(config.get(sect, "tags"))
+        else:
+            self.tags = {}
 
     def split_multipart_metric(self, name):
         m = self.sfx_re.match(name)
@@ -162,12 +181,44 @@ class LibratoStore(object):
 
     def sanitize(self, name):
         return self.sanitize_re.sub("_", name)
+    
+    def parse_tags(self, name, multipart=False):
+        # Find and parse the tags from the name using the syntax name#tag1=value,tag2=value
+        s = name.split("#")
+        tags = {}
+        raw_tags = []
+        
+        if len(s) > 1:
+            name = s.pop(0)
+            raw_tags = s.pop().split(",")
+            if multipart:
+                # Timers will append .p90, .p99 etc to the end of the "metric name"
+                # Parse the suffix out and append to the metric name
+                last_tag = raw_tags.pop()
+                last_tag_split = last_tag.split('.')
+
+                # Store the proper name. The suffix is the last element in split of the last tag.
+                name = name + "." + last_tag_split.pop()
+
+                # Put the tag, without the suffix, back in the list of raw tags
+                if len(last_tag_split) > 1:
+                    t = ".".join(last_tag_split)
+                    # # We had periods in the tag value...
+                    raw_tags.append(t)
+                    # raw_tags.extend(last_tag_split)
+                else:
+                    raw_tags.extend(last_tag_split)
+
+            # Parse the tags out
+            for raw_tag in raw_tags:
+                # Get the key and value from tag=value
+                tag_key, tag_value = raw_tag.split("=")
+                tags[tag_key] = tag_value
+        return name, tags
+                
+        
 
     def add_measure(self, key, value, time):
-        # metric and source names must be 255 or fewer characters
-        if len(key) > 255:
-            key = key[:255]
-
         ts = int(time)
         if self.floor_time_secs != None:
             ts = (ts / self.floor_time_secs) * self.floor_time_secs
@@ -190,6 +241,12 @@ class LibratoStore(object):
                 source = m.group(1)
                 name = name[0:m.start(0)] + name[m.end(0):]
 
+        # Add a source prefix
+        if self.source_prefix:
+            source = "%s.%s" % (self.source_prefix, source)
+
+        # Parse the tags out
+        name, tags = self.parse_tags(name, ismultipart)
         subf = None
         if ismultipart:
             name, subf = self.split_multipart_metric(name)
@@ -199,27 +256,69 @@ class LibratoStore(object):
         # Bail if skipping
         if name == None:
             return
-
+            
         # Add a metric prefix
         if self.prefix:
             name = "%s.%s" % (self.prefix, name)
 
-        # Add a source prefix
-        if self.source_prefix:
-            source = "%s.%s" % (self.source_prefix, source)
-
         name = self.sanitize(name)
-        source = self.sanitize(source)
+        
+        # Add the hostname as a global tag. 
+        self.tags['host'] = self.host
 
-        k = "%s\t%s" % (name, source)
-        if k not in self.gauges:
-            self.gauges[k] = {
-                'name' : name,
-                'source' : source,
-                'measure_time' : ts,
+        if source:
+            # Sanitize
+            source = self.sanitize(source)
+
+            # Add a tag of source if not specified by the client
+            if 'source' not in tags:
+                tags['source'] = source
+
+            # Build a key for the dict that will hold all the measurements to
+            # submit
+            k = "%s\t%s" % (name, source)
+        else:
+            k = name
+
+        if k not in self.measurements:
+            m = [{'name': name, 'tags' : tags, 'time' : ts, subf: value}]
+            self.measurements[k] = m
+        else:
+            # Build summary statistics
+            processed = False
+            # Try to find an existing measurement for this tagset
+            # so we can add the next summary statistic
+            for m in self.measurements[k]:
+                if m['tags'] == tags:
+                    m[subf] = value
+                    processed = True
+                    break
+            if not processed:
+                # New tagset
+                payload = {'name': name, 'tags' : tags, 'time' : ts, subf: value}
+                self.measurements[k].append(payload)
+
+        # Build out the legacy gauges
+        if self.write_to_legacy:
+            if k not in self.gauges:
+                # Truncate metric/source names to 255 for legacy
+                if len(name) > 255:
+                    name = name[:255]
+                    self.logger.warning(
+                        "Truncating metric %s to 255 characters to avoid failing entire payload" % name
+                    )
+                if source and len(source) > 255:
+                    source = source[:255]
+                    self.logger.warning(
+                        "Truncating source %s to 255 characters to avoid failing entire payload" % source
+                    )
+                self.gauges[k] = {
+                    'name': name,
+                    'source': source,
+                    'measure_time': ts
                 }
 
-        self.gauges[k][subf] = value
+            self.gauges[k][subf] = value
 
     def build(self, metrics):
         """
@@ -237,20 +336,32 @@ class LibratoStore(object):
 
             self.add_measure(k, vs, ts)
 
-    def flush_payload(self, headers, g):
+    def flush_payload(self, headers, m, legacy = False):
         """
         POST a payload to Librato.
         """
-
-        body = json.dumps({ 'gauges' : g })
-
-        url = "%s/v1/metrics" % (self.api)
+        
+        if legacy:
+            body = json.dumps({ 'gauges' : m })
+            url = "%s/v1/metrics" % (self.api)
+        else:
+            global_tags = self.tags
+            body = json.dumps({ 'measurements' : m, 'tags': global_tags })
+            url = "%s/v1/measurements" % (self.api)
+                
         req = urllib2.Request(url, body, headers)
 
         try:
             f = urllib2.urlopen(req, timeout = self.flush_timeout_secs)
             response = f.read()
             f.close()
+            # The new tags API supports partial payload accept/reject
+            # So let's show a message if any part fails
+            if 'errors' in response:
+                parsed_response = json.loads(response)
+                # errors could be [], so check that prior to logging anything
+                if parsed_response['errors']:
+                    self.logger.error(parsed_response)
         except urllib2.HTTPError as error:
             body = error.read()
             self.logger.warning('Failed to send metrics to Librato: Code: %d. Response: %s' % \
@@ -269,28 +380,50 @@ class LibratoStore(object):
         """
 
         # Nothing to do
-        if len(self.gauges) == 0:
+        if len(self.measurements) == 0:
             return
-
+    
         headers = {
             'Content-Type': 'application/json',
             'User-Agent': self.build_user_agent(),
             'Authorization': 'Basic %s' % self.build_basic_auth()
             }
 
-        metrics = []
+        tagged_metrics = []
+        legacy_metrics = []
         count = 0
-        for g in self.gauges.values():
-            metrics.append(g)
-            count += 1
+        
+        for v in self.measurements.values():
+            for metric in v:
+                tagged_metrics.append(metric)
+                count += 1
 
-            if count >= self.max_metrics_payload:
-                self.flush_payload(headers, metrics)
-                count = 0
-                metrics = []
+                if count >= self.max_metrics_payload:
+                    self.flush_payload(headers, tagged_metrics)
+                    count = 0
+                    tagged_metrics = []
 
         if count > 0:
-            self.flush_payload(headers, metrics)
+            self.flush_payload(headers, tagged_metrics)
+        
+        # If enabled, submit flush metrics to Librato's legacy API
+        if self.write_to_legacy:
+            if len(self.gauges) == 0:
+                return
+            values = self.gauges.values()
+            count = 0
+            for measure in values:
+                legacy_metrics.append(measure)
+                count += 1
+
+                if count >= self.max_metrics_payload:
+                    self.flush_payload(headers, legacy_metrics, True)
+                    count = 0
+                    legacy_metrics = []
+
+            if count > 0:
+                self.flush_payload(headers, legacy_metrics, True)
+        
 
     def build_basic_auth(self):
         base64string = base64.encodestring('%s:%s' % (self.email, self.token))
@@ -317,7 +450,7 @@ if __name__ == "__main__":
 
     # Get all the inputs
     metrics = sys.stdin.read()
-
+    
     # Flush
     librato.build(metrics.splitlines())
     librato.flush()
