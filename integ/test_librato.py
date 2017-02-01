084483b9207ac068cb55bc17ab79491e0006cb59
Bryan Mikaelian <bryan@librato.com>
2017-02-01 10:09:24 -0600

    Update test

--- integ/test_librato.py
+++ integ/test_librato.py
@@ -29,7 +29,7 @@ timers.query.p95|1017.000000|1401577507
 timers.query.p99|1017.000000|1401577507
 timers.query.rate|16.950000|1401577507
 counts.tags#tag1=value1|16.000000|1401577507
-timers.tags_many#tag1=value1,tag2=value2.sum|1.000000|1401577507\
+timers.tags_many#tag1=value1,tag2=value2.p90|16.000000|1401577507\
     """)
 
     f = tempfile.NamedTemporaryFile(delete=False)
@@ -127,12 +127,11 @@ class TestLibrato(object):
 
     def test_measurements_with_suffix(self):
         expected_output = {
-            "name":         "tags_many",
+            "name":         "tags_many.p90",
             "time":         1401577507,
-            "sum" :         1.0,
+            "value" :         16.0,
             "tags":         { "host": "localhost", "tag1": "value1", "tag2": "value2" }
         }
-        print(self.librato.measurements)
-        
-        assert expected_output == self.librato.measurements["tags_many\tlocalhost"]
+
+        assert expected_output == self.librato.measurements["tags_many.p90\tlocalhost"]
         
